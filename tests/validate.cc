3298858d133c29d4dc9f44432d8f5db014774c01
David Mazieres <dm@uun.org>
2025-06-03 13:47:20 -0700

    Use ADR to find validation functions.

--- tests/validate.cc
+++ tests/validate.cc
@@ -14,13 +14,15 @@ void validate(const fix_4& f4);
 using namespace std;
 using namespace xdr;
 
-namespace xdr {
-template<> void
-validate<fix_4>(const fix_4 &f4)
+namespace testns {
+template<typename T> inline void xdr_validate_enum(T);
+}
+
+void
+validate(const fix_4 &f4)
 {
   if (f4.i == 0)
     throw xdr::xdr_invariant_failed("fix_4::i has value 0");
-}
 }
 
 int
@@ -57,6 +59,19 @@ main()
     ok = true;
   }
   assert (ok);
+
+  v = xdr_to_opaque(uint32_t{99});
+  color c;
+  xdr_from_opaque(v, c);
+
+  testns::other_color oc;
+  ok = false;
+  try {
+    xdr_from_opaque(v, oc);
+  } catch (const xdr::xdr_invariant_failed &) {
+    ok = true;
+  }
+  assert(ok);
 
   return 0;
 }
